4102ddb344125e81f98c5900682bca1141a7723f
CTO - CreativeCoding Solutions <cto@creativecoding.cloud>
2026-06-25 08:29:09 +0000

    fix(LFP): add /demo/search to CSRF except list
    The Kernel.php ValidateCsrfToken::except() was not taking effect
    because Laravel 11 uses bootstrap/app.php middleware definition
    which overrides the Kernel's static call. Added /demo/search to
    the except array in bootstrap/app.php.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->validateCsrfTokens(except: ['webhook/stripe']);
+        $middleware->validateCsrfTokens(except: ['webhook/stripe', '/demo/search']);
         $middleware->replace(
             \Illuminate\Http\Middleware\TrustProxies::class,
             \App\Http\Middleware\TrustProxies::class
